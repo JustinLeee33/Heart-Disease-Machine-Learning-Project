68dc810a220bb9e5361a014c077c9bc1a1f5d54e
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2024-09-09 18:52:05 -0400

    Update download.py

--- src/download.py
+++ src/download.py
@@ -2,15 +2,16 @@ import subprocess
 import zipfile
 import os
 
-# Define the dataset file and directory
-dataset_file = 'heart-disease-data.zip'
-dataset_dir = 'heart-disease-data'
+def download_and_extract_dataset():
+    # Define the dataset file and directory
+    dataset_file = 'heart-disease-data.zip'
+    dataset_dir = 'heart-disease-data'
 
-# Download the dataset from Kaggle
-subprocess.run(['kaggle', 'datasets', 'download', '-d', 'redwankarimsony/heart-disease-data', '--force'], check=True)
+    # Download the dataset from Kaggle
+    subprocess.run(['kaggle', 'datasets', 'download', '-d', 'redwankarimsony/heart-disease-data', '--force'], check=True)
 
-# Unzip the downloaded dataset
-with zipfile.ZipFile(dataset_file, 'r') as zip_ref:
-    zip_ref.extractall(dataset_dir)
+    # Unzip the downloaded dataset
+    with zipfile.ZipFile(dataset_file, 'r') as zip_ref:
+        zip_ref.extractall(dataset_dir)
 
-print(f"Dataset downloaded and extracted to {dataset_dir}")
+    print(f"Dataset downloaded and extracted to {dataset_dir}")
