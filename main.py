a75ebee6ab50e0944c2a8f43fca61c65f9c353f6
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2024-09-09 18:54:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from src.download import download_and_extract_dataset
 
-if __name__ == "__main__":
+def main():
+
+    # Download the Heart Disease Dataset from Kaggle (save to data/heart_disease.csv)
     download_and_extract_dataset()
+
+    # Gather our Data (import the csv)
+
+    # Visualize the Data (save into a folder called data/plots)
+    
+
+if __name__ == "__main__":
+    main()
