afc7261a185ce3594ee04d4d4789aa558c16ba70
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2024-09-17 18:50:15 -0400

    Update random_forest.py

--- src/random_forest.py
+++ src/random_forest.py
@@ -3,7 +3,7 @@ from sklearn.metrics import accuracy_score, classification_report
 import matplotlib.pyplot as plt
 import os
 
-def train_and_evaluate(X_train, X_test, y_train, y_test, plot_dir='data/plots'):
+def rf_train_and_evaluate(X_train, X_test, y_train, y_test, plot_dir='data/plots'):
     """Train and evaluate Random Forest model."""
     model = RandomForestClassifier()
     model.fit(X_train, y_train)
