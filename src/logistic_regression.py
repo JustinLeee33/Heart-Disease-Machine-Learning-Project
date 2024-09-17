a0d7a05a0af8dd3666f00a5bec589c0e8285aa8c
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2024-09-17 18:49:42 -0400

    Update logistic_regression.py

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -3,7 +3,7 @@ from sklearn.metrics import accuracy_score, classification_report
 import matplotlib.pyplot as plt
 import os
 
-def train_and_evaluate(X_train, X_test, y_train, y_test, plot_dir='data/plots'):
+def lr_train_and_evaluate(X_train, X_test, y_train, y_test, plot_dir='data/plots'):
     """Train and evaluate Logistic Regression model."""
     model = LogisticRegression(max_iter=1000)
     model.fit(X_train, y_train)
