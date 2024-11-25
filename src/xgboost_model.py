2468eff21d22dcf3aff6dadbf8877ce5606d4566
JustinLeee33 <justinleethirtythree@gmail.com>
2024-11-24 22:47:20 -0500

    Update xgboost_model.py

--- src/xgboost_model.py
+++ src/xgboost_model.py
@@ -74,13 +74,6 @@ def xgb_train_and_evaluate(X_train, X_test, y_train, y_test, plot_dir='data/plot
     # Ensure plot directory exists
     os.makedirs(plot_dir, exist_ok=True)
 
-    xgb_model = XGBClassifier(
-        # ...your hyperparameters
-        early_stopping_rounds=10,  # Stop if validation score doesn't improve for 10 rounds
-        eval_set=[(X_val, y_val)],  # Validation data
-        eval_metric='logloss',  # Choose your evaluation metric
-    )
-    xgb_model.fit(X_train, y_train)
 
     # Save the classification report
     report_path = os.path.join(plot_dir, 'xgboost_classification_report.txt')
@@ -94,3 +87,12 @@ def xgb_train_and_evaluate(X_train, X_test, y_train, y_test, plot_dir='data/plot
 
     # Return the model, predictions, and predicted probabilities (y_scores)
     return best_model, y_pred, y_scores
+
+     xgb_model = XGBClassifier(
+        # ...your hyperparameters
+        early_stopping_rounds=10,  # Stop if validation score doesn't improve for 10 rounds
+        eval_set=[(X_val, y_val)],  # Validation data
+        eval_metric='logloss',  # Choose your evaluation metric
+    )
+    xgb_model.fit(X_train, y_train)
+
